ed89bd6e56df95003d09f450f6829945c5a19ce2
bryan <bryan.guner@gmail.com>
2021-10-17 23:04:29 -0400

    reorginization

---
+++ scrap.js
@@ -0,0 +1,7 @@
+var img1 = document.querySelector( '.img-1' );
+img1.addEventListener( 'load', function () {
+  // woo yey image loaded
+} );
+img1.addEventListener( 'error', function () {
+  // argh everything's broken
+} );
--- www.rapidtables.com/web/dev/redirect/php-redirect-test.php
+++
@@ -1,5 +0,0 @@
-<?php
-// PHP permanent URL redirection test
-header("Location: http://www.rapidtables.com/web/dev/php-redirect.htm", true, 301);
-exit();
-?>

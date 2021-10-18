20baa0bf6604496263cbe016358dfb1e726e854a
bryan <bryan.guner@gmail.com>
2021-10-17 23:24:58 -0400

    reorginization

--- scrap.js
+++
@@ -1,7 +0,0 @@
-var img1 = document.querySelector( '.img-1' );
-img1.addEventListener( 'load', function () {
-  // woo yey image loaded
-} );
-img1.addEventListener( 'error', function () {
-  // argh everything's broken
-} );
